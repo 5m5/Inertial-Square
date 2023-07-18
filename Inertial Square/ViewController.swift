19f00a0f91b56d3935b76194f815ee1bced618b8
Mikhail Andreev <qr.stronger@gmail.com>
2023-07-18 17:40:14 +0300

    Done sixth task

--- Inertial Square/ViewController.swift
+++ Inertial Square/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+	private lazy var square = makeSquare()
+	private lazy var dynamicAnimator = UIDynamicAnimator(referenceView: view)
+	private lazy var snapBehavior = UISnapBehavior(item: square, snapTo: view.center)
 
-	override func viewDidLoad() {
+	override func viewDidLoad()
+	{
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		view.backgroundColor = .systemBackground
+		view.addSubview(square)
+		dynamicAnimator.addBehavior(snapBehavior)
 	}
 
-
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+	{
+		guard
+			let touchCoordinates = touches.first?.location(in: view)
+		else
+		{
+			return
+		}
+		snapBehavior.snapPoint = touchCoordinates
+	}
 }
 
+private extension ViewController
+{
+	func makeSquare() -> UIView
+	{
+		let size = CGSize(width: 100, height: 100)
+		let square = UIView(frame: .init(origin: view.center, size: size))
+		square.layer.cornerRadius = 8
+		square.backgroundColor = .systemBlue
+		return square
+	}
+}
